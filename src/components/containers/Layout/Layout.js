10013ca3dd1a5efe0cc9c3fc2ed027a86418044c
Vaishnav Nair <64798176+iamvaishnav@users.noreply.github.com>
2021-03-03 16:41:54 +0530

    Update Layout.js

--- src/components/containers/Layout/Layout.js
+++ src/components/containers/Layout/Layout.js
@@ -173,7 +173,7 @@ class Layout extends Component {
         }
 
         return (
-            <section className={[classes.layout, classes['thunderstorm']].join(' ')}>
+            <section className={[classes.layout, classes[weatherImg]].join(' ')}>
                 {weather}
             </section>
         );
