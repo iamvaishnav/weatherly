5ece2fa51fb9a2c6be82b675c96fe6bc66702900
Vaishnav Nair <nairvaishnav007@icloud.com>
2021-03-06 15:36:21 +0530

    change app name

--- src/components/containers/Logo/Logo.js
+++ src/components/containers/Logo/Logo.js
@@ -2,9 +2,9 @@ import React from 'react';
 import classes from './Logo.module.scss';
 
 const logo = (props) => (
-	<div className={classes.logo}>
-		<h4>weatherly</h4>
-	</div>
+    <div className={classes.logo}>
+        <h4>WeatherStore</h4>
+    </div>
 );
 
 export default logo;
